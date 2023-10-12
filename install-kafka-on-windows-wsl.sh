a2c50d6ea99c15e07f2f6188d80ade258f816a6b
Manasa Nandimandalam <nandi_manasa98@yahoo.com>
2023-10-12 18:51:54 -0400

    Update install-kafka-on-windows-wsl.sh

--- install-kafka-on-windows-wsl.sh
+++ install-kafka-on-windows-wsl.sh
@@ -1,2 +1,10 @@
-wget https://downloads.apache.org/kafka/3.5.1/kafka-3.5.1-src.tgz
-tar -xzf kafka-3.5.1-src.tgz
+wget https://archive.apache.org/dist/kafka/3.5.0/kafka-3.5.0-src.tgz
+tar -xzf kafka-3.5.0-src.tgz
+
+#Ensure that the kafka is installed
+~/kafka_2.13-3.5.0/bin/kafka-topics.sh
+
+#set the path in the .bashrc file
+sudo echo PATH="$PATH:~/kafka_2.13-3.5.0/bin/">> .bashrc
+
+#Now you should be able to run kafka binaries without using the entire path.
