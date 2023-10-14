d4d6c6e4b7acd6860565fc078eab183a16b75e9f
Manasa Nandimandalam <nandi_manasa98@yahoo.com>
2023-10-14 10:14:51 -0400

    Update config.py

--- configs/config.py
+++ configs/config.py
@@ -3,8 +3,8 @@ bootstrap_servers = "healthy-lionfish-5018-us1-kafka.upstash.io:9092"
 sasl_mechanism = 'SCRAM-SHA-256'
 security_protocol = 'SASL_SSL'
 sasl_plain_username = 'aGVhbHRoeS1saW9uZmlzaC01MDE4JGxY2ft2THMMCzPDT8YMwZ08-V29NiXB6c8'
-sasl_plain_password = 'MzU1OTg2NDgtMGY1Zi00ZDU4LTkzZGYtMjUwMzViNTcyZTIx'
-sasl_jaas_config= "org.apache.kafka.common.security.scram.ScramLoginModule required username=\"aGVhbHRoeS1saW9uZmlzaC01MDE4JGxY2ft2THMMCzPDT8YMwZ08-V29NiXB6c8\" password=\"MzU1OTg2NDgtMGY1Zi00ZDU4LTkzZGYtMjUwMzViNTcyZTIx\";"
+sasl_plain_password = '**************'
+sasl_jaas_config= "org.apache.kafka.common.security.scram.ScramLoginModule required username=\"aGVhbHRoeS1saW9uZmlzaC01MDE4JGxY2ft2THMMCzPDT8YMwZ08-V29NiXB6c8\" password=\"**********\";"
 # wikimedia config
 wikimedia_stream_url = "https://stream.wikimedia.org/v2/stream/recentchange"
 
